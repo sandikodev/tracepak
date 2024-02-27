80bffb518fe7492c38b218d773eecddc6d9fa319
sandikodev <androxoss@hotmail.com>
2024-02-27 22:15:12 +0700

    create landing page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,112 +1,65 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
-import Counter from "../components/starter/counter/counter";
-import Hero from "../components/starter/hero/hero";
-import Infobox from "../components/starter/infobox/infobox";
-import Starter from "../components/starter/next-steps/next-steps";
-
 export default component$(() => {
   return (
-    <>
-      <Hero />
-      <Starter />
-
-      <div role="presentation" class="ellipsis"></div>
-      <div role="presentation" class="ellipsis ellipsis-purple"></div>
-
-      <div class="container container-center container-spacing-xl">
-        <h3>
-          You can <span class="highlight">count</span>
-          <br /> on me
-        </h3>
-        <Counter />
-      </div>
-
-      <div class="container container-flex">
-        <Infobox>
-          <div q:slot="title" class="icon icon-cli">
-            CLI Commands
+    <div class="flex min-h-screen flex-col">
+      <header class="flex items-center justify-between px-5 py-10">
+        <a href="/">
+          <div class="logo">
+            <h3 class="font-bold">Tracepak</h3>
           </div>
-          <>
-            <p>
-              <code>npm run dev</code>
-              <br />
-              Starts the development server and watches for changes
-            </p>
-            <p>
-              <code>npm run preview</code>
-              <br />
-              Creates production build and starts a server to preview it
-            </p>
-            <p>
-              <code>npm run build</code>
-              <br />
-              Creates production build
-            </p>
-            <p>
-              <code>npm run qwik add</code>
-              <br />
-              Runs the qwik CLI to add integrations
-            </p>
-          </>
-        </Infobox>
-
-        <div>
-          <Infobox>
-            <div q:slot="title" class="icon icon-apps">
-              Example Apps
-            </div>
-            <p>
-              Have a look at the <a href="/demo/flower">Flower App</a> or the{" "}
-              <a href="/demo/todolist">Todo App</a>.
-            </p>
-          </Infobox>
-
-          <Infobox>
-            <div q:slot="title" class="icon icon-community">
-              Community
-            </div>
-            <ul>
-              <li>
-                <span>Questions or just want to say hi? </span>
-                <a href="https://qwik.builder.io/chat" target="_blank">
-                  Chat on discord!
-                </a>
-              </li>
-              <li>
-                <span>Follow </span>
-                <a href="https://twitter.com/QwikDev" target="_blank">
-                  @QwikDev
-                </a>
-                <span> on Twitter</span>
-              </li>
-              <li>
-                <span>Open issues and contribute on </span>
-                <a href="https://github.com/BuilderIO/qwik" target="_blank">
-                  GitHub
-                </a>
-              </li>
-              <li>
-                <span>Watch </span>
-                <a href="https://qwik.builder.io/media/" target="_blank">
-                  Presentations, Podcasts, Videos, etc.
-                </a>
-              </li>
-            </ul>
-          </Infobox>
+        </a>
+        <a href="mailto:androxoss@hotmail.com" data-splitbee-event="Say Hello">
+          <button class="rounded-full bg-gray-200 px-8 py-3 text-sm font-semibold text-gray-800 transition-all  hover:bg-gray-300 focus:bg-gray-400">
+            Mulai Bisnis
+          </button>
+        </a>
+      </header>
+      <section class="mb-4 flex flex-1 flex-col justify-between gap-10 px-5">
+        <div class="mt-24">
+          <h1 class="mb-2 w-fit text-4xl font-bold">Tracepak .</h1>
+          <p class="text-wrap w-80 text-justify">
+            adalah sebuah aplikasi web yang menghubungkan pelanggan dimana saja
+          </p>
         </div>
-      </div>
-    </>
+        <a
+          href="/project"
+          class="mb-24 flex w-full scale-100 flex-col items-center justify-around space-y-10 rounded-xl bg-indigo-600 p-10 transition-transform md:flex-row md:space-y-0 md:hover:-skew-x-3 md:hover:scale-105"
+        >
+          <div class="flex flex-col space-y-2">
+            <span class="text-xl font-bold">
+              Let's Talk about Your Project.
+            </span>
+            <span class="opacity-90">
+              Something on your mind? what you want to build just close by one
+              Click!
+            </span>
+          </div>
+          <button class="flex items-center rounded-full bg-white px-8 py-3 text-sm font-semibold text-gray-800 transition-all hover:bg-gray-300 focus:bg-gray-400">
+            Let's do this
+          </button>
+        </a>
+      </section>
+      <footer class="md:px-auto flex items-end justify-between px-4 pb-4">
+        <ul>
+          <li>address: daguran, trimurti, srandakan, bantul</li>
+          <li>office: kopikonfig.id</li>
+          <li>email: androxoss@hotmail.com</li>
+          <li>phone: 089649246450</li>
+        </ul>
+        <p>created by sandikodev ❤️</p>
+      </footer>
+    </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Tracepak",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "yuk kembangin bisnis!",
     },
   ],
 };
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,10 +1,5 @@
 import { component$, Slot, useStyles$ } from "@builder.io/qwik";
-import { routeLoader$ } from "@builder.io/qwik-city";
 import type { RequestHandler } from "@builder.io/qwik-city";
-
-import Header from "../components/starter/header/header";
-import Footer from "../components/starter/footer/footer";
-
 import styles from "./styles.css?inline";
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
@@ -18,21 +13,14 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
   });
 };
 
-export const useServerTimeLoader = routeLoader$(() => {
-  return {
-    date: new Date().toISOString(),
-  };
-});
-
 export default component$(() => {
   useStyles$(styles);
+
   return (
     <>
-      <Header />
-      <main>
+      <main class="mx-auto min-h-screen max-w-screen-lg">
         <Slot />
       </main>
-      <Footer />
     </>
   );
 });
